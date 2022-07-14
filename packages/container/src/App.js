0fde614aa9bc725a22327760505d93c204eac6cb
Karim Mokhtar <karimmokhtar28@gmail.com>
2022-07-14 02:03:49 +0200

    remove console

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  console.log("container");
   return (
     <div>
       <h1>Hi there!</h1>
