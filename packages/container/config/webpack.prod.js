a0d6c128cf9d987d1bfb33e3c186c81ca5f28995
Karim Mokhtar <karimmokhtar28@gmail.com>
2022-07-21 20:25:55 +0200

    add public path to webpack prod config

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
@@ -10,6 +11,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
